2c137637b6a2d1d09f869169841e64874ce73bbf
Artem Vlasov <artvl@yandex-team.ru>
2017-01-17 01:43:33 +0300

    First task initial

---
+++ src/main/java/bigInt/TestBIgInteger.java
@@ -0,0 +1,103 @@
+package bigInt;
+
+import org.jfree.data.xy.XYSeries;
+import org.jfree.ui.RefineryUtilities;
+
+import java.math.BigInteger;
+import java.util.Random;
+import java.util.function.IntSupplier;
+import java.util.function.Supplier;
+import java.util.stream.IntStream;
+
+/**
+ * Created by artvl on 17.01.17.
+ */
+public class TestBIgInteger {
+    static int cnt = 20000;
+
+    public static void main(String[] args) {
+
+        warmUp();
+
+        long[] arr = new long[cnt];
+        arr[0] = 0;
+
+        Random random = new Random();
+        for (int i = 1; i < cnt; i += 1) {
+
+            IntStream intStream1 = random.ints(0, 10);
+            IntStream intStream2 = random.ints(0, 10);
+            String a = intStream1.limit(i).map(num ->  (char) num ).collect(StringBuilder::new,
+                    (sb, num) -> sb.append(num),
+                    StringBuilder::append
+                    ).toString();
+            String b = intStream2.limit(i).map(num ->  (char) num ).collect(StringBuilder::new,
+                    (sb, num) -> sb.append(num),
+                    StringBuilder::append
+            ).toString();
+//            System.out.println(a);
+//            System.out.println(b);
+
+            BigInteger aa = new BigInteger(a);
+            BigInteger bb = new BigInteger(b);
+
+            long start = System.nanoTime();
+            BigInteger c = aa.multiply(bb);
+            long end = System.nanoTime();
+            arr[i] = end - start;
+            System.out.println(i + " : " + (end - start));
+        }
+
+
+        final XYSeries series = new XYSeries("Random Data");
+        for (int i = 0 ; i < cnt ; i ++) {
+            series.add((double)i, calcAvg(arr, i));
+        }
+        final XYSeriesDemo demo = new XYSeriesDemo("XY Series Demo",series);
+        demo.pack();
+        RefineryUtilities.centerFrameOnScreen(demo);
+        demo.setVisible(true);
+    }
+
+    static void warmUp() {
+        Random random = new Random();
+        for (int i = 1; i < 2000; i ++) {
+
+            IntStream intStream1 = random.ints(0, 10);
+            IntStream intStream2 = random.ints(0, 10);
+            String a = intStream1.limit(i).map(num ->  (char) num ).collect(StringBuilder::new,
+                    (sb, num) -> sb.append(num),
+                    StringBuilder::append
+            ).toString();
+            String b = intStream2.limit(i).map(num ->  (char) num ).collect(StringBuilder::new,
+                    (sb, num) -> sb.append(num),
+                    StringBuilder::append
+            ).toString();
+
+            BigInteger aa = new BigInteger(a);
+            BigInteger bb = new BigInteger(b);
+
+
+            BigInteger c = aa.multiply(bb);
+
+
+        }
+
+    }
+
+    static double calcAvg (long[] arr, int pos) {
+        int size = arr.length;
+        long min = Integer.MAX_VALUE;
+        long max = Integer.MIN_VALUE;
+        long sum = 0;
+        long cnt = 0;
+        for (int i = Math.max(0, pos - 3); i < Math.min(size, pos + 3); i ++) {
+            cnt ++;
+            sum += arr[i];
+            min = arr[i] < min ? arr[i] : min;
+            max = arr[i] > max ? arr[i] : max;
+        }
+        sum -= (max + min);
+        return  (double) sum / (double) (cnt - 2);
+    }
+}
---
+++ src/main/java/bigInt/XYSeriesDemo.java
@@ -0,0 +1,67 @@
+package bigInt;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
+
+
+public class XYSeriesDemo extends ApplicationFrame {
+
+    /**
+     * A demonstration application showing an XY series containing a null value.
+     *
+     * @param title  the frame title.
+     */
+    public XYSeriesDemo(final String title, XYSeries series) {
+
+        super(title);
+
+        final XYSeriesCollection data = new XYSeriesCollection(series);
+        final JFreeChart chart = ChartFactory.createXYLineChart(
+                "XY Series Demo",
+                "X",
+                "Y",
+                data,
+                PlotOrientation.VERTICAL,
+                true,
+                true,
+                false
+        );
+
+        final ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
+        setContentPane(chartPanel);
+
+    }
+
+// ****************************************************************************
+// * JFREECHART DEVELOPER GUIDE                                               *
+// * The JFreeChart Developer Guide, written by David Gilbert, is available   *
+// * to purchase from Object Refinery Limited:                                *
+// *                                                                          *
+// * http://www.object-refinery.com/jfreechart/guide.html                     *
+// *                                                                          *
+// * Sales are used to provide funding for the JFreeChart project - please    *
+// * support us so that we can continue developing free software.             *
+// ****************************************************************************
+
+//    /**
+//     * Starting point for the demonstration application.
+//     *
+//     * @param args  ignored.
+//     */
+//    public static void main(final String[] args) {
+//
+//        final XYSeriesDemo demo = new XYSeriesDemo("XY Series Demo");
+//        demo.pack();
+//        RefineryUtilities.centerFrameOnScreen(demo);
+//        demo.setVisible(true);
+//
+//    }
+
+}
